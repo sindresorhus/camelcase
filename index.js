889dc815458fe6c14969b78acb72456dc97e98a2
Sindre Sorhus <sindresorhus@gmail.com>
2014-10-12 12:58:17 +0200

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = function (str) {
+	return str
+	.trim()
+	.replace(/^[_.\- ]+/, '')
+	.toLowerCase()
+	.replace(/[_.\- ]+(\w|$)/g, function (m, p1) {
+		return p1.toUpperCase();
+	});
+};
---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict';
+var test = require('ava');
+var camelCase = require('./');
+
+test(function (t) {
+	t.assert(camelCase('foo') === 'foo');
+	t.assert(camelCase('foo-bar') === 'fooBar');
+	t.assert(camelCase('foo-bar-baz') === 'fooBarBaz');
+	t.assert(camelCase('foo--bar') === 'fooBar');
+	t.assert(camelCase('--foo-bar') === 'fooBar');
+	t.assert(camelCase('--foo--bar') === 'fooBar');
+	t.assert(camelCase('FOO-BAR') === 'fooBar');
+	t.assert(camelCase('-foo-bar-') === 'fooBar');
+	t.assert(camelCase('--foo--bar--') === 'fooBar');
+	t.assert(camelCase('foo.bar') === 'fooBar');
+	t.assert(camelCase('foo..bar') === 'fooBar');
+	t.assert(camelCase('..foo..bar..') === 'fooBar');
+	t.assert(camelCase('foo_bar') === 'fooBar');
+	t.assert(camelCase('__foo__bar__') === 'fooBar');
+	t.assert(camelCase('__foo__bar__') === 'fooBar');
+	t.assert(camelCase('foo bar') === 'fooBar');
+	t.assert(camelCase('  foo  bar  ') === 'fooBar');
+});
