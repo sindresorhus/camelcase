5c3e94f5a2bb7ccda36d34bf893a555b7b9ea15a
zswang <wangjihu@sensorsdata.cn>
2017-11-05 21:11:08 +0800

    Simplified preserveCamelCase() and Additional test cases

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 'use strict';
 
 function preserveCamelCase(str) {
-	let isLastCharLower = false;
-	let isLastCharUpper = false;
-	let isLastLastCharUpper = false;
-
-	for (let i = 0; i < str.length; i++) {
-		const c = str[i];
-
-		if (isLastCharLower && /[a-zA-Z]/.test(c) && c.toUpperCase() === c) {
-			str = str.substr(0, i) + '-' + str.substr(i);
-			isLastCharLower = false;
-			isLastLastCharUpper = isLastCharUpper;
-			isLastCharUpper = true;
-			i++;
-		} else if (isLastCharUpper && isLastLastCharUpper && /[a-zA-Z]/.test(c) && c.toLowerCase() === c) {
-			str = str.substr(0, i - 1) + '-' + str.substr(i - 1);
-			isLastLastCharUpper = isLastCharUpper;
-			isLastCharUpper = false;
-			isLastCharLower = true;
-		} else {
-			isLastCharLower = c.toLowerCase() === c;
-			isLastLastCharUpper = isLastCharUpper;
-			isLastCharUpper = c.toUpperCase() === c;
-		}
-	}
-
-	return str;
+	return str.replace(/([a-z][^A-Z]*)([A-Z])|([A-Z][^A-Z]*)([A-Z][a-z])/g, (...args) => {
+		return (args[1] || args[3]) + '-' + (args[2] || args[4]);
+	});
 }
 
 module.exports = function (str) {
