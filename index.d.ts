df4ad29c02cae0cd7521454be71d90308c1d499f
rocktimsaikia <srocktim61@gmail.com>
2020-10-06 22:42:47 +0530

    fix example in  declaration file

--- index.d.ts
+++ index.d.ts
@@ -46,7 +46,7 @@ camelCase('--foo.bar', {pascalCase: false});
 //=> 'fooBar'
 
 camelCase('Foo-Bar', {preserveConsecutiveUppercase: true});
-//=> 'FooBAR'
+//=> 'fooBAR'
 
 camelCase('fooBaR', {pascalCase: true, preserveConsecutiveUppercase: true}));
 //=> 'FooBaR'
